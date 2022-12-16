c66624d369abdbd266a5e7a10b8251bbca42b033
Michael Bond <michael@bondcodes.com>
2022-12-15 19:51:15 -0500

    Add some error handling

--- Flammoth.Rcl/Pages/Index.razor.cs
+++ Flammoth.Rcl/Pages/Index.razor.cs
@@ -4,14 +4,28 @@ namespace Flammoth.Rcl.Pages;
 public partial class Index
 {
     private const string PageTitle = "Flammoth";
+    private const string AuthCodeKey = "authCode";
 
     private LoginModel LoginModel { get; } = new() { Instance = @"mastodon.social" };
 
     private AuthModel AuthModel { get; } = new();
+
+    private MastodonList<Status>? HomeTimeline { get; set; }
+
     private AuthenticationClient? authClient;
     private AppRegistration? appRegistration;
     private Auth? auth;
     private MastodonClient? client;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var authToken = await LocalStorageService.GetItemAsStringAsync(AuthCodeKey);
+        if (authToken is { Length: > 0 })
+        {
+            AuthModel.AuthCode = authToken;
+            await AuthorizeAsync();
+        }
+    }
 
     private async Task AuthenticateAsync()
     {
@@ -21,7 +35,7 @@ public partial class Index
         }
 
         authClient = new AuthenticationClient(LoginModel.Instance, HttpClient);
-        appRegistration = await authClient.CreateApp("Flammoth", Scope.Read | Scope.Write | Scope.Follow);
+        appRegistration = await authClient.CreateApp("Flammoth", Scope.Read);
         var url = authClient.OAuthUrl();
         try
         {
@@ -33,19 +47,48 @@ public partial class Index
         }
     }
 
+    private async Task AuthorizeViaFormAsync()
+    {
+        if (appRegistration is null)
+        {
+            return;
+        }
+
+        await AuthorizeAsync();
+    }
+
     private async Task AuthorizeAsync()
     {
-        if (authClient is null || appRegistration is null || AuthModel is not { AuthCode.Length: > 0 })
+        if (LoginModel is not { Instance.Length: > 0 } || authClient is null || AuthModel is not { AuthCode.Length: > 0 })
         {
             return;
         }
 
-        auth = await authClient.ConnectWithCode(AuthModel.AuthCode);
-        if (auth is null)
+        try
         {
+            auth = await authClient.ConnectWithCode(AuthModel.AuthCode);
+            if (auth is null)
+            {
+                return;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
             return;
         }
 
-        client = new MastodonClient("instance", auth.AccessToken, HttpClient);
+        client = new MastodonClient(LoginModel.Instance, auth.AccessToken, HttpClient);
+
+        await DoStuffWithClient();
+    }
+
+    private async Task DoStuffWithClient()
+    {
+        if (client is null)
+        {
+            return;
+        }
+        HomeTimeline = await client.GetPublicTimeline(options: new ArrayOptions { Limit = 10 });
     }
 }
